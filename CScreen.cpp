774f436281770ef23993e57dab4534bb5cb81abd
Daniel Cordell <danielbc@hotmail.co.uk>
2016-03-20 19:47:03 +0000

    Update CScreen.cpp

--- CScreen.cpp
+++ CScreen.cpp
@@ -1,11 +1,5 @@
 #include "Headers/CScreen.h"
 
-void cScreen:: buttonHighlightDetect(sf::Vector2i &mousePos, sf::Text &button) {
-	if (button.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-		button.setColor(sf::Color(200, 200, 200));
-	}
-	else button.setColor(sf::Color::White);
-}
 void cScreen::buttonHighlightDetect(sf::Vector2i &mousePos, sf::Sprite &button) {
 	if (button.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
 		button.setColor(sf::Color(200, 200, 200));
