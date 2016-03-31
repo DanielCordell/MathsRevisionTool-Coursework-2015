772fc74ef74250b9c69bcb2e6ba13cf7b2732f4f
Daniel Cordell <danielbc@hotmail.co.uk>
2016-03-31 10:51:44 +0100

    Update Button.h

--- Headers/Button.h
+++ Headers/Button.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "SFML\Graphics.hpp"
 
-// A class that defines a clickable button with a sprite and a texture
+// A class that defines a clickable button with a sprite and a texture.
+// The LoadTexture function takes a string parameter, which is the location of the file where the texture is stored.
+// This function returns true if texture loading was a success, and false if it has failed. This allows for error detection.
+// The InitSprite function takes that texture and applies it to the sprite so it can be used on the screen.
 
 class Button {
 public:
