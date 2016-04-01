fab7c5857b5dfafb4de31cb1337f7ee3ea068662
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-01 14:13:57 +0100

    Update Rand.h

--- Headers/Rand.h
+++ Headers/Rand.h
@@ -1,9 +1,9 @@
 #pragma once
-// Returns an integer value between min and max inclusive
+// Returns an integer value between min and max inclusive, will be used for random question generators to generate numbers randomly.
 int getRand(int min, int max);
 
-// Returns a random boolean value
+// Returns a random boolean value.
 bool getBool();
 
-// Returns either +1 or -1
+// Returns either +1 or -1, will be used to randomly turn some numbers negative.
 int getPlMn();
