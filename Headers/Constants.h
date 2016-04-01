ed0f129996acc4a200f2c4f707bc874277697e80
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-01 13:25:35 +0100

    Update Constants.h

--- Headers/Constants.h
+++ Headers/Constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// A header file containing game constants
-// Any changes here will affect the rest of the program
+// A header file containing constant global variables that will be used throughout the program
+// In this program the only necessary global variables are the width and height of the window in pixels
 
 const int WINDOW_X = 1280;
 const int WINDOW_Y = 720;
