16fbbcc2c711fadf86796f14ff5f1d98b9f4bbd8
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-01 13:41:42 +0100

    Update GameState.cpp

--- GameState.cpp
+++ GameState.cpp
@@ -8,37 +8,49 @@
 #include "Headers/Screen5.h"
 #include <iostream>
 
-// GameState constructor
+// GameState Constructor, runs when an instance of GameState is created.
 GameState::GameState() {
+	// Creates the window where the game will be displayed. It's size in pixels is WINDOW_X and WINDOW_Y define in the constants
+	// header, the title of the window is "Number Wizard", and sf:Style::Close, which gives the window a titlebar and an exit
+	// button.
 	Window.create(sf::VideoMode(WINDOW_X, WINDOW_Y), "Number Wizard", sf::Style::Close);
+	// This means that all keypresses will be counted as only one press, even if the button is held down.
 	Window.setKeyRepeatEnabled(false);
 }
 
+// This function initialises all game variables and functions. Returns false there is an error, true otherwise.
 bool GameState::initGameState() {
-	// Creates icon used in top left of window
+	// Creates an image class which will store the game icon (shown in the top left of the screen, and in the taskbar).
 	sf::Image icon;
+	
+	// Loads the icon from the icon file. If the icon does not exist, the initGameState function will return false, letting
+	// the program know that there has been an error.
 	if (!icon.loadFromFile("Resources/gameIcon.png")) {
 		return false;
 	}
+	// Sets the icon of the window to the pixel data of the icon. The icon class can now be discarded.
 	Window.setIcon(128, 128, icon.getPixelsPtr());
 
-	// Creates settings file if it doesn't exist and loads the settings from file
+	// Creates settings file if it doesn't exist and loads the settings from file.
 	settings.initSettings();
 
-	// Setup of new screens
-	Screens.push_back(new screen0); // Adding Main Menu
-	Screens.push_back(new screen1); // Adding Options Menu
-	Screens.push_back(new screen2); // Adding Game Screen
-	Screens.push_back(new screen3); // Adding Help Screen
-	Screens.push_back(new screen4); // Adding Score Screen
-	Screens.push_back(new screen5); // Adding Retry Screen
+	// Setup of the list of screens.
+	Screens.push_back(new screen0); // Adding Main Menu to the list
+	Screens.push_back(new screen1); // Adding Options Menu to the list
+	Screens.push_back(new screen2); // Adding Game Screen to the list
+	Screens.push_back(new screen3); // Adding Help Screen to the list
+	Screens.push_back(new screen4); // Adding Score Screen to the list 
+	Screens.push_back(new screen5); // Adding Retry Screen to the list
 
-	// Initialises all screens
+	// Initialises all screens using a for loop. If any of the initialisation functions fails, the initGameState function returns
+	// false.
 	for (unsigned int i = 0; i < Screens.size(); i++) {
 		if (!Screens[i]->Init()) {
 			return false;
 		}
+		// Once the screen is initialised, the internal value of the screen is assigned using this function.
 		Screens[i]->setCurrentScreen(i);
 	}
+	// Initialisation has been successful, return true so the program knows this.
 	return true;
 }
