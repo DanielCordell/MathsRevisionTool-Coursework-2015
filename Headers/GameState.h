e1dcc0dd32bd26e08f963a2a586f3dbeb7da27d7
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-01 13:41:45 +0100

    Update GameState.h

--- Headers/GameState.h
+++ Headers/GameState.h
@@ -4,16 +4,21 @@
 #include "Settings.h"
 
 class GameState {
-public:
+public: // Public Members
+	
+	// GameState Constructor, runs when an instance of GameState is created.
 	GameState();
+	
+	// This function initialises all game variables and functions. Returns false there is an error, true otherwise.
 	bool initGameState();
 
-	// Game Window
+	// The class that refers to the game window.
 	sf::RenderWindow Window;
 
-	// Settings Class
+	// The class that refers to the user's settings. The members of this class are changed via the settings menu.
 	Settings settings;
 
-	// List of screens
+	// A list of all screens (technically a list of pointers to cScreen classes, and as all screen classes inherit from 
+	// cScreen they can all be stored in this list).
 	std::vector<cScreen*> Screens;
 };
