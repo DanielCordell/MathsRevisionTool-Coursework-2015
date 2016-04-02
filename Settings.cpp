8854853dc91449854f255caed3bf1cbf6888e69a
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-02 12:59:24 +0100

    Update Settings.cpp

--- Settings.cpp
+++ Settings.cpp
@@ -3,14 +3,18 @@
 #include <sstream>
 #include <Windows.h>
 
+// Initialising static member variables
 int Settings::highScore;
 int Settings::volumePercent;
 bool Settings::volumeMute;
 std::map<questionTypes, bool> Settings::questionSettings;
 
+// Settings constructor, runs automatically when an instance of the settings class is created.
 Settings::Settings() {
+	// Initialises the file stream by setting the location of the file to be read/written to.
 	settingsFile = std::fstream("Settings/settings.txt");
-
+	
+	// Initialises the list of all of the question settings by adding their enumeration ID and a default value of true.
 	questionSettings.insert(std::make_pair(questionTypes::add, true));
 	questionSettings.insert(std::make_pair(questionTypes::sub, true));
 	questionSettings.insert(std::make_pair(questionTypes::dvd, true));
@@ -21,11 +25,15 @@ Settings::Settings() {
 	questionSettings.insert(std::make_pair(questionTypes::sim, true));
 }
 
+// Settings destructor, runs automatically when an instance of the settings class is destroyed (out of scope). This will only happen
+// when the program closes.
 Settings::~Settings() {
-	//Deletes current settings and saves new settings
+	// Deletes the current settings file, and creates a new file.
 	std::remove("Settings/settings.txt");
 	settingsFile.open("Settings/settings.txt", std::fstream::out);
-
+	
+	// Saves all of the settings to the settings file, in the correct syntax so it can be read again when the program opens.
+	// (the << operator moves a string to a 'stream', in this case the file i/o stream. "\n" is shorthand for newline).
 	settingsFile << "// Volume Settings // \n";
 	settingsFile << "volPer:" << Settings::volumePercent << "\n";
 	settingsFile << "volMut:" << Settings::volumeMute << "\n";
@@ -40,11 +48,15 @@ Settings::~Settings() {
 	settingsFile << "qstQdr:" << questionSettings[questionTypes::qdr] << "\n";
 	settingsFile << "qstSim:" << questionSettings[questionTypes::sim] << "\n";
 	settingsFile << "// End of Settings //";
+	// Closes the settings file, saving all changes.
 	settingsFile.close();
 }
 
+// Generates a settings file with the default values.
 void Settings::generateSettingsFile() {
+	// Creates the file "Settings" if it does not exist already
 	CreateDirectory("Settings", 0);
+	// Creates the settings file if it does not exist, opens it, and loads all default settings values into it.
 	settingsFile.open("Settings/settings.txt", std::fstream::out);
 	settingsFile << "// Volume Settings // \n";
 	settingsFile << "volPer:50\n";
@@ -60,46 +72,47 @@ void Settings::generateSettingsFile() {
 	settingsFile << "qstQdr:1\n";
 	settingsFile << "qstSim:1\n";
 	settingsFile << "// End of Settings //";
-
+	// Saves and closes settings file
 	settingsFile.close();
 }
 
+// Loads a specific setting from the string argument of the function into the member variable that stores that specific setting 
+// in the class.
 void Settings::loadSettingsFromFile(std::string settingsLine) {
-	// Best way I can do this in limited time scale
-
 	// Split the setting into it's name and its value
 	std::string settingName = settingsLine.substr(0, 6);
 	int settingValue;
 	std::istringstream buffer(settingsLine.substr(7));
 	buffer >> settingValue;
-
-	std::cout << "Loading Setting: " << settingsLine << " into " << settingName << " With Value " << settingValue << "\n";
-
-	// If the setting is volume level
+	
+	// If the setting is volume level...
 	if (settingName == "volPer") {
 		volumePercent = settingValue;
 	}
 
-	// If the setting is volume muted
+	// If the setting is volume muted...
 	else if (settingName == "volMut") {
 		volumeMute = settingValue;
 	}
-
+	
+	// If the setting is the high score...
 	else if (settingName == "hghScr") {
 		highScore = settingValue;
 	}
-	// If the setting describes a question being active or not
+	// If the setting describes a question being active or not...
 	else if (questionSettings.find(stringToEnum(settingName)) != questionSettings.end()) {
 		questionSettings[stringToEnum(settingName)] = settingValue;
 	}
 
+	// If the setting does not exist...
 	else {
 		std::cout << "Settings file contains illegal characters:" << settingsLine << "\n" << "Ignoring...\n\n";
 	}
 }
-
+// Initialises the settings class, by creating a settings file if one does not exist already, and loading all settings from the 
+// file into the class.
 void Settings::initSettings() {
-	//If the file does not exist
+	//If the file does not exist then generate a new file.
 	if (!settingsFile.good()) {
 		//Generate Settings File
 		std::cout << "Settings File Does Not Exist... \n";
@@ -113,12 +126,12 @@ void Settings::initSettings() {
 		std::cout << "Settings File Exists... \n\n";
 		settingsFile.close();
 	}
-
+	// Opens the settings file for reading and writing.
 	settingsFile.open("Settings/settings.txt", std::fstream::in);
-	// Read the file line by line
+	// Read the file line by line.
 	std::string settingsLine;
 	while (std::getline(settingsFile, settingsLine)) {
-		// If the line loaded from the file is not blank and is not a comment then load that setting
+		// If the line loaded from the file is not blank and is not a comment (begins with '/') then load that setting
 		if (settingsLine.length() != 0) {
 			if (settingsLine[0] != '/') {
 				loadSettingsFromFile(settingsLine);
@@ -127,9 +140,9 @@ void Settings::initSettings() {
 	}
 	settingsFile.close();
 }
-
+// Converts the identifier found in the settings file into the questionTypes enum, so each setting can be referred to programatically.
 questionTypes Settings::stringToEnum(std::string questionString) {
-	if (questionString == "qstAdd")			return questionTypes::add;
+	if (questionString == "qstAdd")		return questionTypes::add;
 	else if (questionString == "qstSub")	return questionTypes::sub;
 	else if (questionString == "qstMlt")	return questionTypes::mlt;
 	else if (questionString == "qstDvd")	return questionTypes::dvd;
@@ -137,5 +150,5 @@ questionTypes Settings::stringToEnum(std::string questionString) {
 	else if (questionString == "qstMfr")	return questionTypes::mfr;
 	else if (questionString == "qstQdr")	return questionTypes::qdr;
 	else if (questionString == "qstSim")	return questionTypes::sim;
-	else									return questionTypes::add; //Should never happen
+	else					return questionTypes::add; //Should never happen
 }
