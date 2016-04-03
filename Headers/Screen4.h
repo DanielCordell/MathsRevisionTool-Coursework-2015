f40670d611ac32a95194f770c9430143d6b19b1f
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-03 13:29:52 +0100

    Update Screen4.h

--- Headers/Screen4.h
+++ Headers/Screen4.h
@@ -4,12 +4,16 @@
 // Class Declaration for Screen that displays the Score
 class screen4: public cScreen {
 private:
+	// The on screen text representing the screen title.
 	sf::Text screenTitle;
+	// Class representing the button to return the user to the main menu.
 	Button buttonBackToMenu;
-
+	
+	// Text values for the screen.
 	sf::Text finalScoreText;
 	sf::Text livesLeftText;
 	sf::Text newHighScoreText;
+	// The font for the current screen.
 	sf::Font font;
 
 public:
