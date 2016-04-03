91a33c07bb91981aec70bec0b1c6897229a04416
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-03 13:41:31 +0100

    Update Screen4.cpp

--- Screen4.cpp
+++ Screen4.cpp
@@ -12,16 +12,24 @@ bool screen4::Init() {
 		|| !buttonBackToMenu.loadTexture("Resources/buttonReturn.png")) {
 		return false;
 	}
-	//Initialize Background
+	//Initialising Background
 	backgroundSprite.setTexture(backgroundTexture);
 	backgroundSprite.setScale(WINDOW_X / backgroundSprite.getGlobalBounds().width, WINDOW_Y / backgroundSprite.getGlobalBounds().height);
 
-	//Initializing Text Values
+	//Initialising Text Values
 	screenTitle = sf::Text("Game Over", font, 80);
-	finalScoreText = sf::Text("", font, 60);
-	livesLeftText = sf::Text("", font, 60);
+	finalScoreText = sf::Text("Final Score:  " + std::to_string(Score::finalScore), font, 60);
+	livesLeftText = sf::Text("Lives Left:   " + std::to_string(Score::livesRemaining)", font, 60);
 	newHighScoreText = sf::Text("New High\n Score!", font, 80);
-
+	
+	// If the high score was beaten, display beaten high score text. If not, then hide the text.
+	if (Score::didBeatHighScore) {
+		newHighScoreText.setColor(sf::Color::Black);
+	}
+	else {
+		newHighScoreText.setColor(sf::Color::Transparent);
+	}
+	
 	//Positioning Text
 	sf::FloatRect textRect;
 	textRect = screenTitle.getLocalBounds();
@@ -49,12 +57,12 @@ bool screen4::Init() {
 int screen4::Events(sf::RenderWindow & window) {
 	sf::Event event;
 	while (window.pollEvent(event)) {
-		// If escape button pressed
+		// If the escape button pressed...
 		bool escapePressed = event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape;
 		if (event.type == sf::Event::Closed || escapePressed) {
 			return screenQuitGame;
 		}
-		// If mouse button pressed
+		// If the mouse button pressed...
 		if (event.type == sf::Event::MouseButtonPressed) {
 			if (event.mouseButton.button == sf::Mouse::Left) {
 				sf::Vector2f mousePos(sf::Mouse::getPosition(window));
@@ -64,7 +72,7 @@ int screen4::Events(sf::RenderWindow & window) {
 				}
 			}
 		}
-		//If mouse is moved
+		//If the mouse is moved...
 		if (event.type == sf::Event::MouseMoved) {
 			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 			buttonHighlightDetect(mousePos, buttonBackToMenu.sprite);
@@ -86,25 +94,19 @@ void screen4::Draw(sf::RenderWindow & window) {
 }
 
 void screen4::Update() {
-	finalScoreText.setString("Final Score:  " + std::to_string(Score::finalScore));
-	livesLeftText.setString("Lives Left:   " + std::to_string(Score::livesRemaining));
-
-	if (Score::didBeatHighScore) {
-		newHighScoreText.setColor(sf::Color::Black);
-	}
-	else {
-		newHighScoreText.setColor(sf::Color::Transparent);
-	}
+	// No Updates Needed
 }
 
 int screen4::Run(sf::RenderWindow &window) {
 	sf::Clock clock;
-
+	// Loop runs forever until the return statement is met.
 	while (true) {
 		if (clock.getElapsedTime().asMilliseconds() >= 1000.0 / 60.0) /* Framerate Limit */ {
 			clock.restart();
-			int nextScreen = Events(window); // What is the next screen that should be loaded
-			if (nextScreen != getCurrentScreen()) return nextScreen; // Return the next screen to main
+			// Events function defines the next screen that should be loaded
+			int nextScreen = Events(window); 
+			// Return the next screen to the main function if it is different to the current screen.
+			if (nextScreen != getCurrentScreen()) return nextScreen; 
 			Update();
 			Draw(window);
 		}
