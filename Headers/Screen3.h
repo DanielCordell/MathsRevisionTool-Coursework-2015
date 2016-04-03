003b52fa8fa1560f6d84c144a3b7643c527b2122
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-03 14:03:37 +0100

    Update Screen3.h

--- Headers/Screen3.h
+++ Headers/Screen3.h
@@ -4,10 +4,13 @@
 // Class Declaration for Screen that displays the Help Menu
 class screen3: public cScreen {
 private:
+	// The on screen text representing the screen title.
 	sf::Text screenTitle;
+	// The class representing the button to take the user back to the main menu.
 	Button buttonBackToMenu;
+	// The on screen help text. This will contain the entire help text.
 	sf::Text helpText;
-
+	// The font for the current screen.
 	sf::Font font;
 
 public:
