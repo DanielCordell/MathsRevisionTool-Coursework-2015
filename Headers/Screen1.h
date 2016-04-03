f30fb9361bbc2a8dc0a594eed28a3899da424f0d
Daniel Cordell <danielbc@hotmail.co.uk>
2016-04-03 13:00:56 +0100

    Update Screen1.h

--- Headers/Screen1.h
+++ Headers/Screen1.h
@@ -3,25 +3,37 @@
 #include "Settings.h"
 #include "Constants.h"
 
-// Class Declaration for Screen that displays the Options Menu
+// Class Declaration for Screen that displays the settings menu. This inherits from the cScreen class.
 class screen1: public cScreen {
 private:
+	// The on screen text representing the screen title.
 	sf::Text screenTitle;
+	// Class representing the button to return the user back to the main menu.
 	Button buttonBackToMenu;
-
+	// The texture representing the cross from a checkbox
 	sf::Texture buttonCrossTexture;
-
+	
+	// The on screen text that will display the subheading 'volume options'.
 	sf::Text screenTitleVolume;
+	// The on screen text which will identify the volume slider.
 	sf::Text volumeText;
+	// The array of vertices which make up the trianglular shape of the volume slider.
 	sf::VertexArray volumeSliderBack;
+	// The white rectangular slider which the user can adjust to change the volume settings.
 	sf::RectangleShape volumeSlider;
-
+	
+	// The on screen text that will identify the mute checkbox
 	sf::Text muteText;
+	// The rectangular shape which will form the clickable part of the checkbox
 	sf::RectangleShape muteButtonBack;
+	// The checkbox cross which will appear and disappear as needed.
 	sf::Sprite muteButtonCrossSprite;
-
+	
+	// The on screen text that will display the subheading "Enable / Disable Questions"
 	sf::Text screenTitleQuestions;
-
+	
+	// The on screen text which will identify the addition questions checkbox, the rectangular clickable part of the checkbox,
+	// and the checkbox cross. All other checkboxes have the same three parts.
 	sf::Text questionTextAdd;
 	sf::RectangleShape questionButtonAddBack;
 	sf::Sprite questionButtonAddSprite;
@@ -53,7 +65,8 @@ private:
 	sf::Text questionTextSimultaneous;
 	sf::RectangleShape questionButtonSimultaneousBack;
 	sf::Sprite questionButtonSimultaneousSprite;
-
+	
+	// The font used on the current screen.
 	sf::Font font;
 
 	// If true the volume slider is allowed to move
